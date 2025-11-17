7db0f69664d1b250877437729c79ec2bce753c31
Niklas Ruppel <niklas_Ruppel@icloud.com>
2025-11-17 09:31:21 +0100

    Remove StoreConfiguration and add EF Core dependencies
    StoreConfiguration.cs has been removed as it was unused, and several new Entity Framework Core packages (Design, Relational, SqlServer) were added. These updates support advanced EF Core features and better manage database interactions.

--- EvalProG2.Application/obj/Debug/net8.0/EvalProG2.Application.AssemblyInfo.cs
+++ EvalProG2.Application/obj/Debug/net8.0/EvalProG2.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EvalProG2.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41e963d92fddef0ee8fea920a2abc5a4529a7bd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb06d1cc408e204b57fd2066073d4dcd53a06b61")]
 [assembly: System.Reflection.AssemblyProductAttribute("EvalProG2.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EvalProG2.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
