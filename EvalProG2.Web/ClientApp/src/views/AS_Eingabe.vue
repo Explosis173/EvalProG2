f62a44c275239295d6a436a286ad518f9a68aeaa
Astrid <astrid.ballach@krones.com>
2025-11-23 13:06:31 +0100

    style: adjusted style completely, see main.ts

--- EvalProG2.Web/ClientApp/src/views/AS_Eingabe.vue
+++ EvalProG2.Web/ClientApp/src/views/AS_Eingabe.vue
@@ -1,24 +1,8 @@
 <script setup lang="ts">
 //---imports---
-import { createApp } from 'vue';
-import App from "../App.vue";
-import PrimeVue from 'primevue/config';
-import DataTable from 'primevue/datatable';
-import Column from 'primevue/column';
-
-import  Aura from '@primevue/themes/aura'
-import 'primeicons/primeicons.css';
-import Button from 'primevue/button';
+import { ref } from 'vue';
 
 //---variables---
-const app = createApp(App);
-app.use(PrimeVue, {
-    theme: {
-        preset: Aura
-    }
-});
-app.component('Button', Button);
-
 const items = [
   {
     image: 'stadt-regensburg.png',
@@ -27,91 +11,149 @@ const items = [
   }
 ];
 
+const date = ref(null)
+const titlevalue = ref(null)
+const jobvalue = ref(null)
 
 //---functions---
 
-
 </script>
 
-<template #content>
-    <head>
 
-    </head>
-    <body>
+<!--main code-->
+<template #content>
+    <div class="page">
         <header>
             <DataTable :value="items">
                 <Column>
-                <template #body="slotProps">
-                    <div style="display: flex; align-items: center; gap: 1rem;">
-                        <img src="../assets/stadt-regensburg.png" alt="Logo der Stadt Regensburg" class="rgb_logo"/>
-                        <div>
-                            <h1 class="bold">{{ slotProps.data.title }}</h1>
-                            <h2 class="italics">{{ slotProps.data.subtitle }}</h2>
+                    <template #body="slotProps">
+                        <div class="banner">
+                            <img src="../assets/stadt-regensburg.png" alt="Logo der Stadt Regensburg" class="rgb_logo"/>
+                            <div>
+                                <h1 class="bold">{{ slotProps.data.title }}</h1>
+                                <h2 class="italics">{{ slotProps.data.subtitle }}</h2>
+                            </div>
                         </div>
-                    </div>
-                </template>
+                    </template>
                 </Column>
             </DataTable>
         </header>
+        
         <main>
             <section>
                 <form class="form" id="examinerForm" v-on:submit.prevent="">
-                    <div><label class="bold" for="title">Titel des Ausschusses: <input id="title" placeholder="Titel"></input></label></div>
-                    <div><label class="bold" for="job">Zu bewertender Ausbildungsberuf: <input  id="job" placeholder="Ausbildungsberuf"></input></label></div>
-                    <div><label class="bold" for="examDay">Datum des Ausschusses: <input id="examDay" placeholder="Datum"></input></label></div>
+                    <div>
+                        <label class="bold" for="title">Titel des Ausschusses: 
+                            <InputText v-model="titlevalue" type="text" id="title" class="inputfield" placeholder="Titel"></InputText>
+                        </label>
+                    </div>
+                    <div>
+                        <label class="bold" for="job">Zu bewertender Ausbildungsberuf: 
+                            <InputText v-model="jobvalue" type="text" id="job" class="inputfield" placeholder="Ausbildungsberuf"></InputText>
+                        </label>
+                    </div>
+                    <div>
+                        <label class="bold" for="examDay">Datum des Ausschusses: 
+                            <DatePicker v-model="date" dateFormat="dd/mm/yyyy" id="examdate" class="inputfield" placeholder="Datum"/>
+                        </label>
+                    </div>
                     <Button type="submit" label="Anlegen" icon="pi pi-check" iconPos="right" severity="danger" raised/>
                 </form>
             </section>
         </main>
+
         <footer>
             <section>Impressum goes here</section>
         </footer>
-    </body>
+    </div>
 </template>
 
+
 <style>
-    header{
-        display: flex;
-        justify-content: start;
-        color: red;
-        background-color: #EDEDED;
+/*--- css for <> ---*/
+    html, body{
+        height: 100%;
+        margin: 0;
     }
+
     main{
         margin-top: 5%;
         margin-bottom: 5%;
         display:flex;
-        align-self: center;
+        justify-content: center; 
+        align-items: center;
+        flex: 1;
     }
+
     footer{
+        margin-top: auto;
         display: flex;
-        justify-content: end;
+        justify-content: flex-end;
         background-color: #EDEDED;
+        padding: 8px 16px;
+    }
+
+    label{
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-end;
+        text-align: right;
+        font-size: 1.5rem;  
+    }
+
+/*--- css for classes ---*/
+    .banner{
+        display: flex; 
+        align-items: center; 
+        justify-content: start; 
+        gap: 1rem; 
+        color: red; 
+        background-color: #EDEDED;
+        font-size: 1rem;
+    }
+
+    .page{
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
     }
 
     .rgb_logo{
-        width: 20%;
+        width: 15%;
         height: auto;
         padding: 0.5cm;
     }
 
-
-    main {
-        display: flex;
-        justify-content: center; 
-        align-items: center;
-    }
-
-
     .form {
         display: flex;
         flex-direction: column; 
-        gap: 20px;              
+        gap: 20px;            
     }
 
+        .inputfield{
+        flex: 0 0 200px;
+        margin-left: 10px;
+    }
+
+    .p-datatable-thead > tr > th, .p-datatable-tbody > tr > td {
+        padding: 0 !important;
+    }
+
+
+    /*zeichenformatierung */
     .bold{
         font-weight: bold
     }
     .italics{
         font-style: italic;
     }
+
 </style>
+
+/*
+
+notes:
++.+.+.+
+-find way to adjust Button
+
+*/
