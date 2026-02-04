6e27edc4804564a9456d3da1b1568fda1c9305ce
Moritz <Pfeifermoritz04@outlook.de>
2026-02-04 15:43:32 +0100

    Startseite auf "Startseite" umbenannt

--- EvalProG2.Web/ClientApp/src/router/index.ts
+++ EvalProG2.Web/ClientApp/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Startseite from '../views/Startseite.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
    {
      path: '/',
      name: 'home',
-     component: HomeView,
+     component: Startseite,
    },
     // {
     //   path: '/index',
