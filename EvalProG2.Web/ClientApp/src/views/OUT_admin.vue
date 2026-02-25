2a3dbdb6446c0c20a29a0cfec59d11fe7313a610
Moritz <Pfeifermoritz04@outlook.de>
2026-02-25 10:12:39 +0100

    Behobener Zurück Button

--- EvalProG2.Web/ClientApp/src/views/OUT_admin.vue
+++ EvalProG2.Web/ClientApp/src/views/OUT_admin.vue
@@ -68,14 +68,17 @@
                     </router-link>
                 </div>
                 <div class="submitbutton">
-                    <router-link to="./">
-                    <Button label="Zurück" class="showMeButton" severity="danger" raised/>
-                    </router-link>
+
                     <router-link to="/closingremarks">
-                    <Button label="Gesamtergebnis" class="showMeButton" severity="danger" raised/>
+                    <Button label="Gesamtergebnis" class="showMe" severity="danger" raised/>
                     </router-link>
-                </div>  
-            
+                </div>
+               <div class="submitbutton">
+              
+                    <router-link to="./">
+                    <Button label="Zurück" class="showMe" severity="danger" raised/>
+                    </router-link>
+            </div>
             </section>
         </main>
 
